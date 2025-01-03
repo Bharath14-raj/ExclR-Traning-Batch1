8fa7eb88d199e7c8c94d39d630e7b978f64282d5
Bharath14-raj <nampallybharath65@gmail.com>
2025-01-03 12:11:02 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,15 @@
+public class Demo2 {
+    public Demo2() {
+    }
+
+    public static void main(String[] var0) {
+        byte var1 = 5;
+        byte var2 = 6;
+        if (var1 > var2) {
+            System.out.println(var1);
+        } else {
+            System.out.println(var2);
+        }
+
+    }
+}
